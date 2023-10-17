bf4fe76a7e93aae78ea4f94e8c257aa4c1c9f9ce
kimsujin <56665733+kim429@users.noreply.github.com>
2023-10-17 13:03:55 +0900

    Create programmers-lv1-001.py
    프로그래머스 Lv.1 약수의 개수와 덧셈

---
+++ programmers-lv1-001.py
@@ -0,0 +1,13 @@
+def solution(left, right):
+    answer = 0
+    while left!=right+1: #left에서 right까지 반복
+        count = 0 #약수 개수 초기화
+        for i in range(1, left+1): #약수 개수 구하기
+            if left%i ==0: #i가 left로 나누어 떨어진다면
+                count +=1 #약수의 개수 +1
+        if count%2==0: #약수의 개수 홀짝 검사
+            answer +=left #짝수라면 더하고
+        else:
+            answer -=left #홀수면 뺀다.
+        left +=1
+    return answer
